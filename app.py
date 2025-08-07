da6c40d2c7bf33a449601cbd0957b78f2995dd6d
Harshit Mhatre <harshitmhatre526@gmail.com>
2025-08-07 19:20:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ def send_email():
         return jsonify({"success": False, "error": str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
